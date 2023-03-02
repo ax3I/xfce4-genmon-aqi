3153e068717f009bac9ea9b4e28056a20dfb3b60
Aleksandr K <koltovskikh@arrival.com>
2023-03-02 12:43:32 +0400

    fix: python syntax

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ import config
 TOKEN = config.DEFAULT_CONFIG["API_TOKEN"]
 CITY = config.DEFAULT_CONFIG["CITY"]
 
-response = requests.get(f"http://api.waqi.info/feed/{CITY}/?token={TOKEN}", timeout=10)
 
-json_data = json.loads(response.text)
-print(json_data["data"]["aqi"])
+def get_aqi(city, token) -> int:
+    response = requests.get(
+        f"http://api.waqi.info/feed/{city}/?token={token}", timeout=5
+    )
+
+    json_data = json.loads(response.text)
+    aqi = json_data["data"]["aqi"]
+    return print(aqi)
+
+
+def main():
+    get_aqi(city=CITY, token=TOKEN)
+
+
+if __name__ == "__main__":
+    main()
